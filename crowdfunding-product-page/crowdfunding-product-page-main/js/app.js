bc3822ed50c58ff693f22384eafefe1f2d203c15
th-rpy <thamer.saraei@usherbrooke.ca>
2021-06-08 02:58:39 -0400

    ADD:  add modal event

--- crowdfunding-product-page/crowdfunding-product-page-main/js/app.js
+++ crowdfunding-product-page/crowdfunding-product-page-main/js/app.js
@@ -1,1 +1,20 @@
-document.getElementById('modal-success').style.display = 'none';
+// Select reward..
+
+const select_btn = document.querySelector('#btn-25');
+const modal_success = document.querySelector('.modal-success');
+
+select_btn.addEventListener('click', () =>{
+    modal_success.style.display = 'flex';
+})
+
+
+// Close a success modal 
+
+const modal_success1 = document.querySelector('.modal-success');
+const close_btn = document.querySelector('.modal-success');
+close_btn.addEventListener('click', () =>{
+    modal_success.style.display = 'none';
+})
+
+
+
