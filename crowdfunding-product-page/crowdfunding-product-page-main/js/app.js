c96e1f7901aa6854cf2a57942c8c43b47aee038f
th-rpy <thamer.saraei@usherbrooke.ca>
2021-06-08 18:04:00 -0400

    ADD:  add modal event : END

--- crowdfunding-product-page/crowdfunding-product-page-main/js/app.js
+++ crowdfunding-product-page/crowdfunding-product-page-main/js/app.js
@@ -1,13 +1,72 @@
 // Select reward..
+let value = 50000;
+let bracket = 10;
+const days = 64;
+let v = 0;
+let enter  = 0; 
 
-const select_btn = document.querySelector('#btn-25');
+const select_btn25 = document.querySelector('#btn-25');
+const select_btn75 = document.querySelector('#btn-75');
+const h3 = document.querySelector('.inf-div h3');
+const p_green = document.querySelector('.inf-green');
+const b = document.querySelector('.inf-div b');
+const info_p = document.querySelector('.reward-div .info'); 
+const input = document.getElementsByName('input-info');
+const brackets = document.getElementsByName('brackets')[0]
+const select_btn = document.querySelector('.reward-div button');
+const total  = document.querySelector('.section-3 .info h1');
+const progress = document.querySelector('.green-bar');
+progress.style.width = '50%';
 const modal_success = document.querySelector('.modal-success');
 const select_modal = document.querySelector('.select-modal');
+const bookmark = document.querySelector('.bookmarck')
 
+bookmark.addEventListener('click', () =>{
+    bookmark.style.backgroundColor = '#3CBCAB90';
+})
 
-select_btn.addEventListener('click', () => {
+select_btn25.addEventListener('click', () => {
     modal_success.style.display = 'none';
     select_modal.style.display = 'grid';
+    h3.innerHTML = 'Bambo Stand';
+    p_green.innerHTML = 'Pledge $25 or more';
+    b.innerHTML = '101';
+    info_p.innerHTML = "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list."
+    input[0].placeholder = '25';
+    v = 25;
+
+})
+
+select_btn75.addEventListener('click', () => {
+    modal_success.style.display = 'none';
+    select_modal.style.display = 'grid';
+    h3.innerHTML = 'Black Edition Stand';
+    p_green.innerHTML = 'Pledge $75 or more';
+    b.innerHTML = '64';
+    info_p.innerHTML = "You get a Black Special Edition computer stand and a personal thank you.You’ll be added to our Backer member list. Shipping is included."
+    input[0].placeholder = '75';
+    v = 75;
+})
+
+input[0].addEventListener('change', (e)=>{
+  enter = parseInt(e.target.value);
+  value += enter;
+});
+
+select_btn.addEventListener('click', () => {
+    if (parseInt(enter) > v) {
+    total.innerHTML = value;
+    let pr = value * 100 / 100000;
+    input[0].value = '';
+    bracket+=1;
+    brackets.innerHTML = bracket;
+    progress.style.width = pr +'%'
+    modal_success.style.display = 'flex'
+}
+    else {
+        alert('Please enter value more than ' + v + '$');
+        input[0].value = '';
+    }
 })
 
 // Close Select modal
